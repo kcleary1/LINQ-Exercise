4fccb422ee0e515968f53b4d389f7c2050e69923
Kyle Cleary <kscleary11@gmail.com>
2024-07-12 13:45:00 -0600

    exercise complete

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+           var videoGames = new List<string>() { "Doom", "Legend of Kyrandia", "NBA Jam", "Mortal Kombat" };
+           
+           var sortedGames = videoGames.OrderBy(title => title.Length).ToList();
+        
+           foreach (var title in  sortedGames)
+            {
+                Console.WriteLine(title);
+            }
+        
+        
+        
         }
     }
 }
